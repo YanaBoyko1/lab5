ad8d72f7d52da909d0dd7502fad0abc7db2bdc09
YanaBoyko1 <150178023+YanaBoyko1@users.noreply.github.com>
2023-12-07 19:26:46 +0200

    Fixed indentations in get_info_of_group method

--- lab5_code.py
+++ lab5_code.py
@@ -31,12 +31,13 @@ class Group:
         else:
             print(f"Student {student.name} is not in the group.")
 
-   def get_info_of_group(self):
-       if not self.students:
-          print("The group is empty.")
+    def get_info_of_group(self):
+        if not self.students:
+           print("The group is empty.")
         else:
-            for student in self.students:
-               print(student)
+          for student in self.students:
+            print(student)
+   
 
 if __name__ == '__main__':
     studentAndy = Student("Andy", "Boyko", [3, 4, 5, 89])
